2eddb68b600a128044146af3a9fc32015a3ac638
Simon Marlow <marlowsd@gmail.com>
2009-03-23 15:20:28 +0000

    new GHC build system: clean up

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-
-TOP=../..
-ENABLE_SHELL_WRAPPERS = YES
-BINDIST_EXTRAS += template-hsc.h
-
-include $(TOP)/mk/boilerplate.mk
-include $(TOP)/mk/cabal.mk
-
+dir = utils/hsc2hs
+TOP = ../..
+include $(TOP)/mk/sub-makefile.mk
--- ghc.mk
+++ ghc.mk
@@ -1,51 +1,34 @@
+utils/hsc2hs_USES_CABAL = YES
+utils/hsc2hs_PACKAGE = hsc2hs
 
-# ToDo: maybe use ghc-cabal to configure this stuff and build
-# Paths_hsc2hs for us.
-utils/hsc2hs_dist_MODULES = Main Paths_hsc2hs
-utils/hsc2hs_dist-install_MODULES = $(utils/hsc2hs_dist_MODULES)
-utils/hsc2hs_VERSION = 0.67
-
-ifeq "$(Windows)" "YES"
 utils/hsc2hs_dist_PROG         = $(GHC_HSC2HS_PGM)
 utils/hsc2hs_dist-install_PROG = $(GHC_HSC2HS_PGM)
-$(HSC2HS_INPLACE) : inplace/hsc2hs-$(utils/hsc2hs_VERSION)/template-hsc.h
 
-utils/hsc2hs_template=inplace/hsc2hs-$(utils/hsc2hs_VERSION)/template-hsc.h
+utils/hsc2hs_dist_SHELL_WRAPPER = YES
+utils/hsc2hs_dist_INSTALL_SHELL_WRAPPER = NO
 
-else
-utils/hsc2hs_dist_PROG         = hsc2hs-real
-utils/hsc2hs_dist-install_PROG = hsc2hs-real
-
-utils/hsc2hs_template=inplace/lib/hsc2hs-$(utils/hsc2hs_VERSION)/template-hsc.h
-
-$(HSC2HS_INPLACE) : $(INPLACE_BIN)/hsc2hs-real \
-	            $(utils/hsc2hs_template)
-	$(RM) -f $@
-	echo '#!$(SHELL)'  >> $@
-	echo 'executablename=$(FPTOOLS_TOP_ABS)/$(INPLACE_BIN)/hsc2hs-real' >>$@
-	echo 'datadir=$(FPTOOLS_TOP_ABS)/inplace/lib/hsc2hs-$(utils/hsc2hs_VERSION)' >>$@
-	cat utils/hsc2hs/hsc2hs.wrapper >>$@
-	$(EXECUTABLE_FILE) $@
-endif
-
+utils/hsc2hs_dist-install_SHELL_WRAPPER = YES
+utils/hsc2hs_dist-install_INSTALL_SHELL_WRAPPER = YES
 utils/hsc2hs_dist-install_INSTALL_INPLACE = NO
-utils/hsc2hs_dist-install_INSTALL = YES
+
 $(eval $(call build-prog,utils/hsc2hs,dist,0))
 $(eval $(call build-prog,utils/hsc2hs,dist-install,1))
+
+# After build-prog above
+utils/hsc2hs_dist_MODULES += Paths_hsc2hs
+utils/hsc2hs_dist-install_MODULES = $(utils/hsc2hs_dist_MODULES)
+
+utils/hsc2hs_template=$(INPLACE_LIB)/template-hsc.h
+
+$(HSC2HS_INPLACE) : $(utils/hsc2hs_template)
 
 $(utils/hsc2hs_template) : utils/hsc2hs/template-hsc.h
 	@$(MKDIRHIER) $(dir $@)
 	$(CP) $< $@
 
+install: install_utils/hsc2hs_dist_install
 
-$(utils/hsc2hs_dist_depfile) : utils/hsc2hs/dist/build/Paths_hsc2hs.hs
-$(utils/hsc2hs_dist-install_depfile) : utils/hsc2hs/dist-install/build/Paths_hsc2hs.hs
-
-utils/hsc2hs/%/build/Paths_hsc2hs.hs : utils/hsc2hs/ghc.mk $(MKDIRHIER)
-	@$(RM) $@
-	@$(MKDIRHIER) $(dir $@)
-	echo "module Paths_hsc2hs where" >>$@
-	echo "import Data.Version" >>$@
-	echo "getDataFileName s = return s" >>$@
-	echo "version = Version [$(subst .,$(comma),$(utils/hsc2hs_VERSION))] []" >>$@
-
+.PHONY: install_utils/hsc2hs_dist_install
+install_utils/hsc2hs_dist_install: utils/hsc2hs/template-hsc.h
+	$(MKDIRHIER $(datadir)
+	$(CP) $< $(datadir)
